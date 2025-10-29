d568a24ed6bd877afd72a9427171e8e2bb95ed99
jebiyeon02 <jhje5595@naver.com>
2025-10-29 22:02:25 +0900

    feat: 올바른 로또 구입 금액을 입력받아 구매 개수를 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  validateInputMoney(inputMoney) {
+    if (isNaN(inputMoney))
+      throw new Error('[ERROR] 구입 금액에는 숫자가 들어와야 합니다.');
+    if (Number(inputMoney) <= 0)
+      throw new Error('[ERROR] 구입 금액은 양의 정수이어야 합니다.');
+    if (Number(inputMoney) % 1000 !== 0)
+      throw new Error('[ERROR] 구입 금액은 1000으로 나누어 떨어져야 합니다.');
+  }
+  async run() {
+    let inputMoney;
+    while (true) {
+      try {
+        inputMoney = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+        this.validateInputMoney(inputMoney);
+
+        break;
+      } catch (error) {
+        Console.print(error);
+      }
+    }
+
+    const boughtLottoNumber = Number(inputMoney) / 1000;
+    Console.print(`${boughtLottoNumber}개를 구매했습니다.`);
+  }
 }
 
 export default App;
