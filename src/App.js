00a1939cdb1da0326027d7c7dfcedab02f3a8445
jebiyeon02 <jhje5595@naver.com>
2025-10-29 22:43:21 +0900

    feat: 로또 개수만큼 오름차순으로 로또 번호 생성 후 각 로또의 번호 출력

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
 
 class App {
   validateInputMoney(inputMoney) {
@@ -9,6 +10,18 @@ class App {
     if (Number(inputMoney) % 1000 !== 0)
       throw new Error('[ERROR] 구입 금액은 1000으로 나누어 떨어져야 합니다.');
   }
+
+  createLottosWithBoughtCount(boughtCount) {
+    const lottos = [];
+    for (let i = 0; i < boughtCount; i += 1) {
+      const lottoNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      lottoNumbers.sort((a, b) => a - b);
+      lottos.push(new Lotto(lottoNumbers));
+    }
+
+    return lottos;
+  }
+
   async run() {
     let inputMoney;
     while (true) {
@@ -16,14 +29,21 @@ class App {
         inputMoney = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
         this.validateInputMoney(inputMoney);
 
+        Console.print('');
         break;
       } catch (error) {
         Console.print(error);
       }
     }
 
-    const boughtLottoNumber = Number(inputMoney) / 1000;
-    Console.print(`${boughtLottoNumber}개를 구매했습니다.`);
+    const boughtLottoCount = Number(inputMoney) / 1000;
+    Console.print(`${boughtLottoCount}개를 구매했습니다.`);
+
+    const lottos = this.createLottosWithBoughtCount(boughtLottoCount);
+    lottos.forEach((lotto) => {
+      Console.print(`[${lotto.getNumbers().join(', ')}]`);
+    });
+    Console.print('');
   }
 }
 
