0c4758122a6b849b06b69551b70dc3600da10a1a
jebiyeon02 <jhje5595@naver.com>
2025-10-29 23:15:31 +0900

    feat: 당첨 번호를 입력받아 검증 실시

--- src/App.js
+++ src/App.js
@@ -22,6 +22,42 @@ class App {
     return lottos;
   }
 
+  removeWhiteSpaceAndSplitAnswerNumbers(answerNumbers) {
+    return answerNumbers.replaceAll(' ', '').split(',');
+  }
+
+  validateAnswerNumbers(answerNumbers) {
+    if (answerNumbers.length !== 6) {
+      throw new Error('[ERROR] 당첨 번호는 6개여야 합니다.');
+    }
+    if (answerNumbers.includes('')) {
+      throw new Error('[ERROR] 당첨 번호에는 빈 값이 존재할 수 없습니다.');
+    }
+
+    answerNumbers.forEach((number) => {
+      if (isNaN(number)) {
+        throw new Error('[ERROR] 당첨 번호에는 숫자가 와야 합니다.');
+      }
+    });
+
+    answerNumbers.forEach((number) => {
+      if (Number(number) < 1 || Number(number) > 45) {
+        throw new Error('[ERROR] 당첨 번호는 1~45사이여야 합니다.');
+      }
+    });
+
+    answerNumbers.forEach((number) => {
+      if (Number(number) % 1 !== 0) {
+        throw new Error('[ERROR] 당첨 번호는 정수만 가능합니다.');
+      }
+    });
+
+    const setAnswerNumbers = new Set(answerNumbers);
+    if (answerNumbers.length !== setAnswerNumbers.size) {
+      throw new Error('[ERROR] 당첨 번호는 중복될 수 없습니다.');
+    }
+  }
+
   async run() {
     let inputMoney;
     while (true) {
@@ -32,7 +68,7 @@ class App {
         Console.print('');
         break;
       } catch (error) {
-        Console.print(error);
+        Console.print(error.message);
       }
     }
 
@@ -44,6 +80,22 @@ class App {
       Console.print(`[${lotto.getNumbers().join(', ')}]`);
     });
     Console.print('');
+
+    let answerNumbers;
+    while (true) {
+      try {
+        const inputAnswerNumbers =
+          await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+        answerNumbers =
+          this.removeWhiteSpaceAndSplitAnswerNumbers(inputAnswerNumbers);
+        this.validateAnswerNumbers(answerNumbers);
+
+        Console.print('');
+        break;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
   }
 }
 
