2d65cc3b4167fee0851b6e26c3fad5eaff76ef00
jebiyeon02 <jhje5595@naver.com>
2025-10-29 23:23:40 +0900

    feat: 보너스 번호를 입력받아 검증 실시

--- src/App.js
+++ src/App.js
@@ -58,6 +58,31 @@ class App {
     }
   }
 
+  removeWhiteSpaceBonusNumber(bonusNumber) {
+    return bonusNumber.replaceAll(' ', '');
+  }
+  validateBonusNumber(bonusNumber, answerNumbers) {
+    if (bonusNumber.includes(' ')) {
+      throw new Error('[ERROR] 보너스 번호에는 빈 값이 존재할 수 없습니다.');
+    }
+
+    if (isNaN(bonusNumber)) {
+      throw new Error('[ERROR] 보너스 번호에는 숫자가 와야 합니다.');
+    }
+
+    if (Number(bonusNumber) < 1 || Number(bonusNumber) > 45) {
+      throw new Error('[ERROR] 보너스 번호는 1~45사이여야 합니다.');
+    }
+
+    if (Number(bonusNumber) % 1 !== 0) {
+      throw new Error('[ERROR] 보너스 번호는 정수만 가능합니다.');
+    }
+
+    if (answerNumbers.includes(bonusNumber)) {
+      throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
+    }
+  }
+
   async run() {
     let inputMoney;
     while (true) {
@@ -89,6 +114,21 @@ class App {
         answerNumbers =
           this.removeWhiteSpaceAndSplitAnswerNumbers(inputAnswerNumbers);
         this.validateAnswerNumbers(answerNumbers);
+
+        Console.print('');
+        break;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+
+    let bonusNumber;
+    while (true) {
+      try {
+        const inputBonusNumber =
+          await Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+        bonusNumber = this.removeWhiteSpaceBonusNumber(inputBonusNumber);
+        this.validateBonusNumber(bonusNumber, answerNumbers);
 
         Console.print('');
         break;
