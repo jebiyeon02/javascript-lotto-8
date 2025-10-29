b7a3d47aa80579bdd433d7cba8a04d1a90a94322
jebiyeon02 <jhje5595@naver.com>
2025-10-29 22:41:51 +0900

    feat: 로또 번호 예외처리 추가 및 로또번호 getter 생성

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,22 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+
+    numbers.forEach((number) => {
+      if (Number(number) < 1 || Number(number) > 45) {
+        throw new Error('[ERROR] 로또 번호는 1~45사이여야 합니다.');
+      }
+    });
+
+    const setNumbers = new Set(numbers);
+    if (numbers.length !== setNumbers.size) {
+      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
+    }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
