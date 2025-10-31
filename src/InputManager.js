ec761d61e5ba457f57a482c53a06856f20d37754
jebiyeon02 <jhje5595@naver.com>
2025-10-31 22:47:38 +0900

    refactor: 당첨 번호 입력 메서드를 InputManager 클래스로 이동

--- src/InputManager.js
+++ src/InputManager.js
@@ -31,6 +31,68 @@ class InputManager {
     if (Number(inputPurchaseAmount) % 1000 !== 0)
       throw new Error('[ERROR] 구입 금액은 1000으로 나누어 떨어져야 합니다.');
   }
+
+  static async inputAnswerNumbersUntilValid() {
+    let answerNumbers;
+    while (true) {
+      try {
+        answerNumbers = await InputManager.#inputAnswerNumbers();
+        Console.print('');
+        break;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+    answerNumbers.sort();
+    return answerNumbers;
+  }
+
+  static async #inputAnswerNumbers() {
+    const inputAnswerNumbers =
+      await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+
+    const answerNumbers =
+      InputManager.#removeWhiteSpaceAndSplitAnswerNumbers(inputAnswerNumbers);
+    InputManager.#validateAnswerNumbers(answerNumbers);
+
+    return Array.from(answerNumbers, (number) => Number(number));
+  }
+
+  static #removeWhiteSpaceAndSplitAnswerNumbers(answerNumbers) {
+    return answerNumbers.replaceAll(' ', '').split(',');
+  }
+
+  static #validateAnswerNumbers(answerNumbers) {
+    if (answerNumbers.length !== 6) {
+      throw new Error('[ERROR] 당첨 번호는 6개여야 합니다.');
+    }
+    if (answerNumbers.includes('')) {
+      throw new Error('[ERROR] 당첨 번호에는 빈 값이 존재할 수 없습니다.');
+    }
+
+    answerNumbers.forEach((number) => {
+      if (isNaN(number)) {
+        throw new Error('[ERROR] 당첨 번호에는 숫자가 와야 합니다.');
+      }
+    });
+
+    answerNumbers.forEach((number) => {
+      if (Number(number) < 1 || Number(number) > 45) {
+        throw new Error('[ERROR] 당첨 번호는 1~45사이여야 합니다.');
+      }
+    });
+
+    answerNumbers.forEach((number) => {
+      if (Number(number) % 1 !== 0) {
+        throw new Error('[ERROR] 당첨 번호는 정수만 가능합니다.');
+      }
+    });
+
+    const setAnswerNumbers = new Set(answerNumbers);
+    if (answerNumbers.length !== setAnswerNumbers.size) {
+      throw new Error('[ERROR] 당첨 번호는 중복될 수 없습니다.');
+    }
+  }
 }
 
 export default InputManager;
