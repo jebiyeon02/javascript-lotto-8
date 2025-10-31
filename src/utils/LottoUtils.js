90f29914bbdf3ec13f5f83ba696aab8d95898ffa
jebiyeon02 <jhje5595@naver.com>
2025-11-01 01:01:14 +0900

    refactor: 로또 구매 수량으로 로또 객체 생성하는 메서드 추가

--- src/utils/LottoUtils.js
+++ src/utils/LottoUtils.js
@@ -1,6 +1,20 @@
+import { Random } from '@woowacourse/mission-utils';
+import Lotto from '../Lotto.js';
+
 class LottoUtils {
   static getPurchaseLottoCount(purchaseAmount) {
     return purchaseAmount / 1000;
+  }
+
+  static createLottosWithPurchaseCount(purchaseCount) {
+    const lottos = [];
+    for (let i = 0; i < purchaseCount; i += 1) {
+      const lottoNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      lottoNumbers.sort((a, b) => a - b);
+      lottos.push(new Lotto(lottoNumbers));
+    }
+
+    return lottos;
   }
 }
 
