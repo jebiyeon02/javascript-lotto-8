4f3827d018f2b129940bcf9fa357b95d98d7a215
jebiyeon02 <jhje5595@naver.com>
2025-11-01 01:04:21 +0900

    refactor: 로또 객체생성 메서드, 보너스 번호 입력 메서드 분리 및 lottoGame 객체 생성

--- src/App.js
+++ src/App.js
@@ -1,53 +1,18 @@
-import { Console, Random } from '@woowacourse/mission-utils';
-import Lotto from './Lotto.js';
+import { Console } from '@woowacourse/mission-utils';
 import LottoResult from './LottoResult.js';
 import InputManager from './InputManager.js';
 import LottoUtils from './utils/LottoUtils.js';
+import LottoGame from './LottoGame.js';
+import BonusNumberInputManager from './InputManager/BonusNumberInputManager.js';
 
 class App {
-  createLottosWithBoughtCount(boughtCount) {
-    const lottos = [];
-    for (let i = 0; i < boughtCount; i += 1) {
-      const lottoNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
-      lottoNumbers.sort((a, b) => a - b);
-      lottos.push(new Lotto(lottoNumbers));
-    }
-
-    return lottos;
-  }
-
-  removeWhiteSpaceBonusNumber(bonusNumber) {
-    return bonusNumber.replaceAll(' ', '');
-  }
-  validateBonusNumber(bonusNumber, answerNumbers) {
-    if (bonusNumber.includes(' ')) {
-      throw new Error('[ERROR] 보너스 번호에는 빈 값이 존재할 수 없습니다.');
-    }
-
-    if (isNaN(bonusNumber)) {
-      throw new Error('[ERROR] 보너스 번호에는 숫자가 와야 합니다.');
-    }
-
-    if (Number(bonusNumber) < 1 || Number(bonusNumber) > 45) {
-      throw new Error('[ERROR] 보너스 번호는 1~45사이여야 합니다.');
-    }
-
-    if (Number(bonusNumber) % 1 !== 0) {
-      throw new Error('[ERROR] 보너스 번호는 정수만 가능합니다.');
-    }
-
-    if (answerNumbers.includes(Number(bonusNumber))) {
-      throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
-    }
-  }
-
   async run() {
     const purchaseAmount = await InputManager.inputPurchaseAmountUntilValid();
 
     const purchaseLottoCount = LottoUtils.getPurchaseLottoCount(purchaseAmount);
     Console.print(`${purchaseLottoCount}개를 구매했습니다.`);
 
-    const lottos = this.createLottosWithBoughtCount(purchaseLottoCount);
+    const lottos = LottoUtils.createLottosWithPurchaseCount(purchaseLottoCount);
     lottos.forEach((lotto) => {
       Console.print(`[${lotto.getNumbers().join(', ')}]`);
     });
@@ -55,20 +20,12 @@ class App {
 
     const answerNumbers = await InputManager.inputAnswerNumbersUntilValid();
 
-    let bonusNumber;
-    while (true) {
-      try {
-        const inputBonusNumber =
-          await Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
-        bonusNumber = this.removeWhiteSpaceBonusNumber(inputBonusNumber);
-        this.validateBonusNumber(bonusNumber, answerNumbers);
+    // answerNumbers가 해당 클래스 내부에서 필요하기 때문에 이 InputManager만 인스턴스 생성
+    const bonusNumberInputManager = new BonusNumberInputManager(answerNumbers);
+    const bonusNumber =
+      await bonusNumberInputManager.inputBonusNumberUntilValid();
 
-        Console.print('');
-        break;
-      } catch (error) {
-        Console.print(error.message);
-      }
-    }
+    const lottoGame = new LottoGame(lottos, answerNumbers, bonusNumber);
 
     const gameResult = new LottoResult(
       lottos,
