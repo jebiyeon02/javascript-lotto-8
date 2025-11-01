6997a1f1710fe3d5bb9c00b304a4b6c4aee3afa2
jebiyeon02 <jhje5595@naver.com>
2025-11-01 19:41:57 +0900

    refactor: 수익률 계산 메서드 추가

--- src/utils/LottoUtils.js
+++ src/utils/LottoUtils.js
@@ -16,6 +16,18 @@ class LottoUtils {
 
     return lottos;
   }
+
+  static getRateOfReturn(winCount, purchaseAmount) {
+    return (
+      ((5000 * winCount.fifth +
+        50000 * winCount.fourth +
+        1500000 * winCount.third +
+        30000000 * winCount.second +
+        2000000000 * winCount.first) /
+        purchaseAmount) *
+      100
+    );
+  }
 }
 
 export default LottoUtils;
