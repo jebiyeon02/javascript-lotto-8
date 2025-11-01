54734fdff7cb6a1a48dc7aa28eea9a4520d77eba
jebiyeon02 <jhje5595@naver.com>
2025-11-01 23:51:36 +0900

    refactor: 하드 코딩된 숫자 상수화

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,3 +1,5 @@
+import { INITIAL_WIN_COUNT, WIN_UNIT_INCREASE } from './constants/Enum';
+
 class LottoGame {
   #lottos; // 로또 번호 배열이 아닌 로또 객체들
   #answerNumbers;
@@ -9,16 +11,16 @@ class LottoGame {
     this.#answerNumbers = answerNumbers;
     this.#bonusNumber = bonusNumbers;
     this.#winCount = {
-      first: 0,
-      second: 0,
-      third: 0,
-      fourth: 0,
-      fifth: 0,
+      first: INITIAL_WIN_COUNT.FIRST,
+      second: INITIAL_WIN_COUNT.SECOND,
+      third: INITIAL_WIN_COUNT.THIRD,
+      fourth: INITIAL_WIN_COUNT.FOURTH,
+      fifth: INITIAL_WIN_COUNT.FIFTH,
     };
   }
 
   addWinCount(lottoRank) {
-    this.#winCount[lottoRank] += 1;
+    this.#winCount[lottoRank] += WIN_UNIT_INCREASE;
   }
 
   getLottos() {
