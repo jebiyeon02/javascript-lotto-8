2db2194632eff0d0f6da3a6b868981d4a043b042
jebiyeon02 <jhje5595@naver.com>
2025-11-01 19:41:44 +0900

    refactor: 출력 담당 클래스 생성

---
+++ src/View/LottoOutputView.js
@@ -0,0 +1,25 @@
+import { Console } from '@woowacourse/mission-utils';
+class LottoOutputView {
+  static printPurchaseLottoCount(purchaseLottoCount) {
+    Console.print(`${purchaseLottoCount}개를 구매했습니다.`);
+  }
+
+  static printLottoNumbers(lottos) {
+    lottos.forEach((lotto) => {
+      Console.print(`[${lotto.getNumbers().join(', ')}]`);
+    });
+  }
+  static printLottoResult(winCount, rateOfReturn) {
+    Console.print('당첨 통계\n---');
+    Console.print(`3개 일치 (5,000원) - ${winCount.fifth}개`);
+    Console.print(`4개 일치 (50,000원) - ${winCount.fourth}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${winCount.third}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${winCount.second}개`,
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${winCount.first}개`);
+    Console.print(`총 수익률은 ${rateOfReturn.toFixed(1)}%입니다.`);
+  }
+}
+
+export default LottoOutputView;
