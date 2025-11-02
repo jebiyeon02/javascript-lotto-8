49d1b137b979a9bbb4daf2261c154b90b31a348d
jebiyeon02 <jhje5595@naver.com>
2025-11-02 19:00:50 +0900

    refactor: 출력값에 개행문자 추가

--- src/View/LottoOutputView.js
+++ src/View/LottoOutputView.js
@@ -1,7 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 class LottoOutputView {
   static printPurchaseLottoCount(purchaseLottoCount) {
-    Console.print(`${purchaseLottoCount}개를 구매했습니다.`);
+    Console.print(`\n${purchaseLottoCount}개를 구매했습니다.`);
   }
 
   static printLottoNumbers(lottos) {
@@ -11,7 +11,7 @@ class LottoOutputView {
   }
 
   static printLottoResult(winCount, rateOfReturn) {
-    Console.print('당첨 통계\n---');
+    Console.print('\n당첨 통계\n---');
     Console.print(`3개 일치 (5,000원) - ${winCount.fifth}개`);
     Console.print(`4개 일치 (50,000원) - ${winCount.fourth}개`);
     Console.print(`5개 일치 (1,500,000원) - ${winCount.third}개`);
--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,7 +1,7 @@
 const MESSAGES = {
   INPUT_BONUS_NUMBER_MESSAGE: '보너스 번호를 입력해 주세요.\n',
-  INPUT_PURCHASE_AMOUNT_MESSAGE: '입금액을 입력해 주세요.\n',
-  INPUT_ANSWER_NUMBER_MESSAGE: '당첨 번호를 입력해 주세요.\n',
+  INPUT_PURCHASE_AMOUNT_MESSAGE: '구입금액을 입력해 주세요.\n',
+  INPUT_ANSWER_NUMBER_MESSAGE: '\n당첨 번호를 입력해 주세요.\n',
 };
 
 export default MESSAGES;
