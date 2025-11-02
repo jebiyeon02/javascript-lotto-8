0b9a99d644e3e105b08bafa8f4b64276759302a6
jebiyeon02 <jhje5595@naver.com>
2025-11-02 18:57:51 +0900

    test: 모든 순위 기능 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -90,4 +90,48 @@ describe('로또 테스트', () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
     });
   });
+
+  test('기능 테스트 - 모든 순위 테스트', async () => {
+    // given
+    const logSpy = getLogSpy();
+
+    mockRandoms([
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 4, 5, 7],
+      [1, 2, 3, 4, 5, 8],
+      [1, 2, 3, 4, 10, 11],
+      [1, 2, 3, 10, 11, 12],
+      [10, 11, 12, 13, 14, 15],
+    ]);
+    mockQuestions(['6000', '1,2,3,4,5,6', '7']);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    const logs = [
+      '6개를 구매했습니다.',
+      '[1, 2, 3, 4, 5, 6]',
+      '[1, 2, 3, 4, 5, 7]',
+      '[1, 2, 3, 4, 5, 8]',
+      '[1, 2, 3, 4, 10, 11]',
+      '[1, 2, 3, 10, 11, 12]',
+      '[10, 11, 12, 13, 14, 15]',
+      '3개 일치 (5,000원) - 1개',
+      '4개 일치 (50,000원) - 1개',
+      '5개 일치 (1,500,000원) - 1개',
+      '5개 일치, 보너스 볼 일치 (30,000,000원) - 1개',
+      '6개 일치 (2,000,000,000원) - 1개',
+      '총 수익률은 33859250.0%입니다.',
+    ];
+
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test('예외 테스트', async () => {
+    await runException('1000j');
+  });
 });
